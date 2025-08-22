9d047e1355dddd4084a56f1adfb812b5cb7e21ee
macielrc <macielrc3@gmail.com>
2025-08-22 04:08:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,17 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('conquestrun-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/app.js',
-        '/manifest.webmanifest'
-      ]);
-    })
-  );
-});
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+const CACHE_NAME = 'conquestrun-demo-v2';
+const URLS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS))
   );
 });
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))\n  );\n});\nself.addEventListener('fetch', (event) => {\n  event.respondWith(\n    caches.match(event.request).then(resp => resp || fetch(event.request))\n  );\n});\n
